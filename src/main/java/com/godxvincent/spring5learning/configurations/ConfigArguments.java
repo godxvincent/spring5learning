c2604bf37b61adad153b1094adc8fa5410f02508
godxvincent <godxvincent@gmail.com>
2021-04-06 20:13:42 -0500

    :alembic: Definición de un bean para manipular los argumentos (opciones) de arranque.

---
+++ src/main/java/com/godxvincent/spring5learning/configurations/ConfigArguments.java
@@ -0,0 +1,21 @@
+package com.godxvincent.spring5learning.configurations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.stereotype.Component;
+
+//https://docs.spring.io/spring-boot/docs/current/reference/html/spring-boot-features.html#boot-features-application-arguments
+@Component
+public class ConfigArguments {
+
+    private final ApplicationArguments args;
+
+    @Autowired
+    public ConfigArguments(ApplicationArguments args) {
+        this.args = args;
+        System.out.println("Estos son las opciones con las que se ejecuto la aplicación");
+        System.out.println(args.getNonOptionArgs());
+
+
+    }
+}
